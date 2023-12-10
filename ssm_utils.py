b2ea1dab157021bd10c8c7bc0a88a2d101498c20
Finlay Miller <finlay@dal.ca>
2023-12-10 14:12:09 -0400

    new output

--- ssm_utils.py
+++ ssm_utils.py
@@ -12,7 +12,7 @@ from typing import Tuple, Dict
 def boundary_split(array, indices):
     """chatgpt-generated"""
     # Check if the indices are valid
-    if any(index < 0 or index >= len(array) for index in indices):
+    if any(index < 0 or index > len(array) for index in indices):
         raise ValueError("Invalid index in the list.")
 
     # Add the start and end indices to the list
