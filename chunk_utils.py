51676ca9a08aed720e57c8b69a7b19aa54550174
Finlay Miller <finlay@dal.ca>
2024-01-09 12:29:08 -0400

    collected all functions into one

--- chunk_utils.py
+++ chunk_utils.py
@@ -2,6 +2,51 @@
 
 import math
 import pretty_midi
+import numpy as np
+
+from typing import Dict
+
+################################  all in one  #################################
+
+
+def all_metrics(midi: pretty_midi.PrettyMIDI, config) -> Dict:
+    num_bins = int(math.ceil(midi.get_end_time() / config["bin_length"]))
+    metrics = {
+        "pitch_histogram": midi.get_pitch_class_histogram(),
+        "tempo": midi.estimate_tempo(),
+        "file_len": midi.get_end_time(),
+        "note_count": sum(len(instrument.notes) for instrument in midi.instruments),
+        "velocities": [{"total_velocity": 0, "count": 0} for _ in range(num_bins)],
+        "lengths": [0.0] * num_bins,
+        "energies": [0.0] * num_bins,
+        "simultaneous_counts": [0] * num_bins,
+    }
+
+    # metrics that are calculated from notes
+    for instrument in midi.instruments:
+        for note in instrument.notes:
+            start_bin = int(note.start // config["bin_length"])
+            end_bin = int(note.end // config["bin_length"])
+            metrics["lengths"].append(note.end - note.start)
+
+            for bin in range(start_bin, min(end_bin + 1, num_bins)):
+                metrics["velocities"][bin]["total_velocity"] += note.velocity
+                metrics["velocities"][bin]["count"] += 1
+                metrics["simultaneous_counts"][bin] += 1
+
+    metrics["lengths"] = sum(metrics["lengths"]) / len(metrics["lengths"])
+
+    # metrics that are calculated from other metrics
+    metrics["energies"] = [
+        config["w1"] * (vel["total_velocity"] / vel["count"])
+        + config["w2"] * metrics["lengths"]
+        for vel in metrics["velocities"]
+    ]
+
+    return metrics
+
+
+############################  individual functions  ###########################
 
 
 def average_note_length(midi: pretty_midi.PrettyMIDI) -> float:
@@ -44,7 +89,7 @@ def total_number_of_notes(midi: pretty_midi.PrettyMIDI) -> int:
 
 
 def total_velocity(
-    midi: pretty_midi.PrettyMIDI, bin_length: float = 1.0
+    midi: pretty_midi.PrettyMIDI, bin_length=None
 ) -> list[dict[str, int]]:
     """
     Calculate the total velocity of all notes for each time bin in a MIDI file.
@@ -56,6 +101,8 @@ def total_velocity(
     Returns:
     list: A list whose values are the total velocity and number of notes in that bin.
     """
+    if bin_length == None:
+        bin_length = midi.get_end_time()
     num_bins = int(math.ceil(midi.get_end_time() / bin_length))
     bin_velocities = [{"total_velocity": 0, "count": 0} for _ in range(num_bins)]
 
@@ -71,9 +118,7 @@ def total_velocity(
     return bin_velocities
 
 
-def simultaneous_notes(
-    midi: pretty_midi.PrettyMIDI, bin_length: float = 1.0
-) -> list[int]:
+def simultaneous_notes(midi: pretty_midi.PrettyMIDI, bin_length=None) -> list[int]:
     """
     Calculate the number of simultaneous notes being played each second in a MIDI file.
 
@@ -84,6 +129,8 @@ def simultaneous_notes(
     Returns:
     list: A list whose values are the number of simultaneous notes being played in that second.
     """
+    if bin_length == None:
+        bin_length = midi.get_end_time()
     num_bins = int(math.ceil(midi.get_end_time() / bin_length))
     simultaneous_notes_counts = [0] * num_bins
 
@@ -102,8 +149,20 @@ def energy(
     midi: pretty_midi.PrettyMIDI,
     w1: float = 0.5,
     w2: float = 0.5,
-    bin_length: float = 1.0,
-):
+    bin_length=None,
+) -> list[float]:
+    """
+    Calculate the number of simultaneous notes being played each second in a MIDI file.
+
+    Parameters:
+    midi (PrettyMIDI): The prettyMIDI container object for the MIDI file.
+    bin_length (float): The length of time each bin should occupy. (default is clip length)
+
+    Returns:
+    list: A list whose values are the number of simultaneous notes being played in that second.
+    """
+    if bin_length == None:
+        bin_length = midi.get_end_time()
     num_bins = int(math.ceil(midi.get_end_time() / bin_length))
     energies = [0.0] * num_bins
     v = total_velocity(midi, bin_length)
@@ -120,3 +179,7 @@ def energy(
                 )
 
     return energies
+
+
+def norm(data):
+    return (data - np.min(data)) / (np.max(data) - np.min(data))
